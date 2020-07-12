bb4cd4df92756a32b328237d723e3008a43a15ff
Maciel-ifpi <68089677+Maciel-ifpi@users.noreply.github.com>
2020-07-12 00:24:00 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, } from 'react-native';
+import {FontAwesome} from '@expo/vector-icons';
 
-export default function App() {
+
+
+const FlatListBasics = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+        <View style={styles.cabeca}>
+           <Text style={styles.texto}>ListView</Text>
+        </View>
+
+        <View style={styles.container}>
+        <FlatList
+        data={[
+          <View><Text style={styles.item}><FontAwesome name = 'book' style={styles.imag}/>  Disciplina</Text></View>,
+          <View><Text style={styles.item}><FontAwesome name = 'globe' style={styles.imag}/>  Normas da ABNT</Text></View>,
+          <View><Text style={styles.item}><FontAwesome name = 'tasks' style={styles.imag}/>  Repositórios</Text></View>,
+          <View><Text style={styles.item}><FontAwesome name = 'cubes' style={styles.imag}/>  Ferramentas</Text></View>,
+          <View><Text style={styles.item}><FontAwesome name = 'mortar-board' style={styles.imag}/>  Orientações</Text></View>,
+          <View><Text style={styles.item}><FontAwesome name = 'question' style={styles.imag}/>  Sugestões</Text></View>,
+        ]}
+        renderItem={({item}) => item}
+      />
+        </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex: 1,
+   paddingTop: 22
+  },
+
+  item: {
+    backgroundColor: '#E4EBEE',
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    marginBottom: 3,
+  },
+
+  cabeca:{
+     backgroundColor: 'green',
+     height: 65,     
+     justifyContent: 'center'
+  },
+
+  texto:{    
+    padding: 10,
+    color: 'white',
+    fontSize: 20,
+  },
+
+  imag:{  
+    marginRight: 20,
+    fontSize: 20,
   },
 });
+
+export default FlatListBasics;
